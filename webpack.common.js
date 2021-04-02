1855f227065a87cf9cef14b61a54aa321ee6e8cd
Teh|Striker <kristianhanus@gmail.com>
2021-04-02 20:47:57 +0200

    Add MiniCssExtractPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports =  {
     entry: './src/app.js',
@@ -16,10 +17,27 @@ module.exports =  {
             {
                 test: /\.s?css$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
                 ]
             }]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+        }),
+    ]
 };
